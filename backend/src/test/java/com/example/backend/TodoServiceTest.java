a50231e875f372f6240cbb0eabe177cc12f0870d
Ben-21 <bs@schaefer-inet.de>
2023-06-17 11:39:55 +0200

    implemented test returnAllTodos() in TodoServiceTest

---
+++ backend/src/test/java/com/example/backend/TodoServiceTest.java
@@ -0,0 +1,36 @@
+package com.example.backend;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+public class TodoServiceTest {
+
+   TodoRepo todoRepo = mock(TodoRepo.class);
+   TodoService todoService = new TodoService(todoRepo);
+
+
+    @Test
+    void returnAllTodos() {
+        //GIVEN
+        Todo todo1 = new Todo("OPEN", "Do the dishes");
+        Todo todo2 = new Todo("OPEN", "Do the floor cleaning");
+        List<Todo> expectedList = new ArrayList<>(List.of(todo1, todo2));
+
+
+        //WHEN
+        when(todoRepo.getAllTodos()).thenReturn(expectedList);
+        List<Todo> actualList = todoService.getAllTodos();
+
+        //THEN
+        Assertions.assertEquals(expectedList, actualList);
+        verify(todoRepo).getAllTodos();
+    }
+
+
+}
